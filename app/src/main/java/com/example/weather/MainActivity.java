06b8ee2d31f220895bd424784cd0a1502c31cd9c
Yaekamishima <evm29500@kwansei.ac.jp>
2019-07-10 02:26:09 +0900

    三田の気温を表示するアプリ完成しました

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,79 +1,31 @@
 package com.example.weather;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.graphics.Typeface;
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+/**
+ * トップ画面を制御するActivityクラス.
+ */
+public class MainActivity extends AppCompatActivity {
 
-        public class MainActivity extends AppCompatActivity {
+    /**
+     * 画面を表示する.
+     * note:デフォルトで実装されている
+     *
+     * @param savedInstanceState savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            // Project Created by Ferdousur Rahman Shajib
-            // www.androstock.com
-
-            TextView temperature, humidity_field, pressure_field;
-            /* Please Put your API KEY here */
-            //String OPEN_WEATHER_MAP_API = "6fed7f8a8b65d9b677317a8833be2d0c";
-            /* Please Put your API KEY here */
-            //http://api.openweathermap.org/data/2.5/weather?lat=34.8899&lon=135.2254
-
-
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-
-                temperature = (TextView) findViewById(R.id.temperature);
-                humidity_field = (TextView) findViewById(R.id.humidity_field);
-                pressure_field = (TextView) findViewById(R.id.pressure_field);
-
-
-            }
-
-
-
-            class DownloadWeather extends AsyncTask < String, Void, String > {
-                @Override
-                protected void onPreExecute() {
-                    super.onPreExecute();
-
-                }
-                protected String doInBackground(String...args) {
-                    String xml = Function.excuteGet("http://api.openweathermap.org/data/2.5/weather?34.8899&lon=135.2254&units=metric&appid=6fed7f8a8b65d9b677317a8833be2d0c");
-                    return xml;
-                }
-                @Override
-                protected void onPostExecute(String xml) {
-
-                    try {
-                        JSONObject json = new JSONObject(xml);
-                        if (json != null) {
-                            //JSONObject details = json.getJSONArray("weather").getJSONObject(0);
-                            JSONObject main = json.getJSONObject("main");
-
-                            temperature.setText(String.format("%.2f", "三田の天気は"+main.getDouble("temp")) + "°");
-                            humidity_field.setText("Humidity: " + main.getString("humidity") + "%");
-                            pressure_field.setText("Pressure: " + main.getString("pressure") + " hPa");
-                        }
-                    } catch (JSONException e) {
-                        Toast.makeText(getApplicationContext(), "Error, Check City", Toast.LENGTH_SHORT).show();
-                    }
-
-
-                }
-
-
-
-            }
-
-
-
+        try {
+            new AsyncHttpRequest(this).execute(new URL("https://api.openweathermap.org/data/2.5/weather?lat=34.8899&lon=135.2254&units=metric&appid=6fed7f8a8b65d9b677317a8833be2d0c"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
+    }
+}
